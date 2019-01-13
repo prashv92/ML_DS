c09e6223796b723f309bc409d62cca962674b7a3
prashv92 <prashv92@utexas.edu>
2019-01-13 17:33:19 -0500

    Adding base model class

---
+++ model.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+class Model():
+    
+    def __init__(data):
+        self.data = data
+        
+        
+    def transform_data():
+        return 0
+    
+    
+    def fit_model():
+        return 0
+        
