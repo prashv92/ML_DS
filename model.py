c2a727a4b3e857ab824a755009a62997a010a2f4
prashv92 <prashv92@utexas.edu>
2019-01-13 17:37:24 -0500

    Updating base model class

--- model.py
+++ model.py
@@ -5,11 +5,11 @@ class Model():
     def __init__(data):
         self.data = data
         
-        
+    @abstractmethod
     def transform_data():
         return 0
     
-    
+    @abstractmethod
     def fit_model():
         return 0
         
