512063b74cb04207d8f46a7ea7ac1318ab910afe
prashv92 <prashv92@utexas.edu>
2019-01-13 22:51:47 -0500

    Clean up

--- RandomForestModel.py
+++ RandomForestModel.py
@@ -11,10 +11,10 @@ class RandomForestModel(Model):
     
     def __init__(self, data, classify):
         self.data = data
-        self.classfiy = classify
+        self.classify = classify
         #super().__init__()
         
-    
+              
     def transform_data(self):
         for c in self.data.columns:
             if self.data[c].dtype == 'object':
@@ -31,16 +31,14 @@ class RandomForestModel(Model):
             
         #x_train, x_test, y_train, y_test = self.split_data(self.data, 0.3)
         if None != self.model:
-            self.model.fit(self.x_train, self.x_test)
+            self.model.fit(self.x_train, self.y_train)
         else:
             print('Model uninstantiated. Please check model')
         
-        return model
+        return self.model
     
     def split_data(self,y,percent):
         self.x_train, self.x_test, self.y_train, self.y_test  = train_test_split(
             self.data[self.data.columns.difference([str(y)])], self.data[str(y)],test_size=percent, random_state=42)
-        
-        #return x_train, x_test, y_train, y_test
-    
+            
     
